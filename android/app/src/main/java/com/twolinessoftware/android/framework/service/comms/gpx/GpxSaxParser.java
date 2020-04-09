9c18c380f725ec905125ac6b0871fda8f5ae53b3
mao.buidinh@hotmail.com <123456?Za>
2020-04-09 12:03:30 +0700

    New GPXSaxParse implement.

--- android/app/src/main/java/com/twolinessoftware/android/framework/service/comms/gpx/GpxSaxParser.java
+++ android/app/src/main/java/com/twolinessoftware/android/framework/service/comms/gpx/GpxSaxParser.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2011 2linessoftware.com
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,111 +15,102 @@
  */
 package com.twolinessoftware.android.framework.service.comms.gpx;
 
-import java.io.StringReader;
+import com.twolinessoftware.android.framework.service.comms.Parser;
 
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
-import org.xml.sax.Attributes;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.DefaultHandler;
-
-import com.twolinessoftware.android.framework.service.comms.Parser;
-
 public class GpxSaxParser extends Parser {
 
-	private GpxSaxParserListener listener;
+    private GpxSaxParserListener listener;
 
-	public GpxSaxParser(GpxSaxParserListener listener) {
-		this.listener = listener;
-	}
+    public GpxSaxParser(GpxSaxParserListener listener) {
+        this.listener = listener;
+    }
 
-	@Override
-	public void parse(String xml) {
+    @Override
+    public void parse(String xml) {
+    }
 
-		try {
+    @Override
+    public void parseFromFile(File file) {
+        SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+        try {
+            SAXParser saxParser = saxParserFactory.newSAXParser();
+            GpxHandler handler = new GpxHandler();
+            saxParser.parse(file, handler);
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+            if (listener != null) listener.onGpxError(e.getMessage());
+        }
 
-			String data = getNodesFromXml(xml, "trk");
+    }
 
-			SAXParserFactory spf = SAXParserFactory.newInstance();
-			SAXParser sp = spf.newSAXParser();
+    class GpxHandler extends DefaultHandler {
 
-			XMLReader xr = sp.getXMLReader();
-			xr.setFeature("http://xml.org/sax/features/namespaces", false);
-			/* Create a new ContentHandler and apply it to the XML-Reader */
-			GpxHandler gpxHandler = new GpxHandler();
-			xr.setContentHandler(gpxHandler);
+        private GpxTrackPoint point;
+        private String currentTag;
 
-			xr.parse(new InputSource(new StringReader(data)));
-		
+        @Override
+        public void startDocument() throws SAXException {
+            if (listener != null)
+                listener.onGpxStart();
+        }
 
-		} catch (Exception e) {
-			if (listener != null)
-				listener.onGpxError(e.getMessage());
+        @Override
+        public void characters(char[] ch, int start, int length)
+                throws SAXException {
+            if (currentTag != null) {
+                String value = new String(ch, start, length);
 
-		}
-	}
+                if (currentTag.equalsIgnoreCase("ele"))
+                    point.setEle(Float.parseFloat(value));
+                else if (currentTag.equalsIgnoreCase("time"))
+                    point.setTime(value);
+                else if (currentTag.equalsIgnoreCase("sat"))
+                    point.setSat(value);
+                else if (currentTag.equalsIgnoreCase("fix"))
+                    point.setFix(value);
+            }
+        }
 
-	class GpxHandler extends DefaultHandler {
+        @Override
+        public void startElement(String uri, String localName, String qName,
+                                 Attributes attributes) throws SAXException {
+            if (qName.equalsIgnoreCase("trkpt")) {
+                point = new GpxTrackPoint();
+                point.setLat(Float.parseFloat(attributes.getValue("lat")));
+                point.setLon(Float.parseFloat(attributes.getValue("lon")));
+                currentTag = qName;
+            } else {
+                currentTag = null;
+            }
+        }
 
-		private GpxTrackPoint point;
-		private String currentTag;
+        @Override
+        public void endElement(String uri, String localName, String qName)
+                throws SAXException {
 
-		@Override
-		public void startDocument() throws SAXException {
-			if (listener != null)
-				listener.onGpxStart();
-		}
+            currentTag = null;
+            if (qName.equalsIgnoreCase("trkpt")) {
+                if (listener != null)
+                    listener.onGpxPoint(point);
+            }
+        }
 
-		@Override
-		public void characters(char[] ch, int start, int length)
-				throws SAXException {
-			if (currentTag != null) {
-				String value = new String(ch, start, length);
-
-				if (currentTag.equalsIgnoreCase("ele"))
-					point.setEle(Float.parseFloat(value));
-				else if (currentTag.equalsIgnoreCase("time"))
-					point.setTime(value);
-				else if (currentTag.equalsIgnoreCase("sat"))
-					point.setSat(value);
-				else if (currentTag.equalsIgnoreCase("fix"))
-					point.setFix(value);
-			}
-		}
-
-		@Override
-		public void startElement(String uri, String localName, String qName,
-				Attributes attributes) throws SAXException {
-			if (qName.equalsIgnoreCase("trkpt")) {
-				point = new GpxTrackPoint();
-				point.setLat(Float.parseFloat(attributes.getValue("lat")));
-				point.setLon(Float.parseFloat(attributes.getValue("lon")));
-
-			}
-
-			currentTag = qName;
-		}
-
-		@Override
-		public void endElement(String uri, String localName, String qName)
-				throws SAXException {
-
-			currentTag = null;
-			if (qName.equalsIgnoreCase("trkpt")) {
-				if (listener != null)
-					listener.onGpxPoint(point);
-			}
-		}
-
-		@Override
-		public void endDocument() throws SAXException {
-			if (listener != null)
-				listener.onGpxEnd();
-		}
-
-	}
+        @Override
+        public void endDocument() throws SAXException {
+            if (listener != null)
+                listener.onGpxEnd();
+        }
+    }
 
 }
